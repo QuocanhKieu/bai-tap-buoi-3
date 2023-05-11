06befff79153093b4a3529ca9f46b8bb969aa889
QuocanhKieu <44871775+QuocanhKieu@users.noreply.github.com>
2023-05-11 11:43:27 +0700

    Add files via upload

---
+++ Tam giac.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{	
+	float height = 0;
+	float bottom = 0;
+    printf("Nhap vao chieu cao tam giac\n");
+    scanf("%f", &height);
+    printf("Nhap vao canh day tam giac\n");
+    scanf("%f", &bottom);
+    printf("S tam giac = %.2f", height*bottom/2);
+    return 0;
+}
+
