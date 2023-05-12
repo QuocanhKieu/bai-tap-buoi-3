7076e94040726e663159109c20e7ac74bf86d391
Quocanh Kieu <kieuquocanh4@gmail.com>
2023-05-13 00:22:49 +0700

    some message

---
+++ %i vs %d.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    scanf("%d %i", &a, &b);
+    printf("a = %d, b = %i", a, b);
+
+    return 0;
+}
+
+
